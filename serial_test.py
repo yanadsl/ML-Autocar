a93dfcd252f0825948e422c65481a7a450f8b9f2
yanadsl <yanadsl1@hotmail.com>
2017-04-30 12:44:02 +0800

    correct errors

--- serial_test.py
+++ serial_test.py
@@ -10,13 +10,14 @@ pi = pigpio.pi()
 sensor_message_size = 7
 sensor_signal_pin = 4
 dead_pin = 17
+sensor_unusable_diff = 4
 pi.set_mode(sensor_signal_pin, pigpio.OUTPUT)
 h1 = pi.serial_open("/dev/ttyAMA0", 9600)
 pi.serial_write_byte(h1, 10 * 2)
 pi.write(sensor_signal_pin, pigpio.LOW)
 print("start")
 sita = 1
-sets = []
+distance = []
 try:
     while True:
         while (int(time.time() * 1000) - time_record) <= time_limit:
@@ -29,26 +30,28 @@ try:
             time.sleep(0.0007)
         (b, d) = pi.serial_read(h1, sensor_message_size)
         pi.write(sensor_signal_pin, pigpio.LOW)
-        sets.clear()
+        distance = []
         for a in d:
-            sets.append(int(a) / 2.0)
+            distance.append(int(a) / 2.0)
         if pi.read(dead_pin) == pigpio.LOW:
             print("dead")
 
-        if (abs(sets[2] - sets[1]) < 7 and sets[2] < 40) or (abs(sets[4] - sets[5]) < 7 and set[4] < 40):
-            print("FIXED")
-            if sets[2] < 40:
-                a = sets[1]+0.5
-                b = sets[2]
+        if (abs(distance[2] - distance[1]) < sensor_unusable_diff and distance[2] < 40) or (
+                abs(distance[4] - distance[5]) < sensor_unusable_diff and distance[4] < 40):
+            print("修正啦FIXED")
+            if distance[2] < 40:
+                a = distance[1] + 0.5
+                b = distance[2]
             else:
-                a = sets[5]+0.5
-                b = sets[4]
+                a = distance[5] + 0.5
+                b = distance[4]
             c = math.sqrt(a ** 2 + b ** 2 - 2 * a * b * math.cos(math.pi * 25 / 180))
             sita = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))
             ans = a * math.sin(math.pi - sita) / math.sin(sita - math.pi * 25 / 180)
-            sets[3] = round(ans, 1)
+            distance[3] = round(ans, 1)
 
-        print([sets[0], sets[1], sets[2], sets[3], sets[4], sets[5], sets[6]], round(math.degrees(sita), 1))
+        print([distance[0], distance[1], distance[2], distance[3], distance[4], distance[5], distance[6]],
+              '      ', round(math.degrees(sita), 1))
         # distance = normalize(distance)
 except KeyboardInterrupt:
     pi.serial_close(h1)
