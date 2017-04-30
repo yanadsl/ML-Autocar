f1074e6987d5659d82c9f9cb3c492a6233ccc66c
yanadsl <yanadsl1@hotmail.com>
2017-04-30 11:58:03 +0800

    correct errors

--- serial_test.py
+++ serial_test.py
@@ -15,6 +15,7 @@ h1 = pi.serial_open("/dev/ttyAMA0", 9600)
 pi.serial_write_byte(h1, 10 * 2)
 pi.write(sensor_signal_pin, pigpio.LOW)
 print("start")
+sita = 1
 try:
     while True:
         while (int(time.time() * 1000) - time_record) <= time_limit:
@@ -36,7 +37,7 @@ try:
 
         if not (abs(sets[2] - sets[1]) > 7 and abs(sets[4] - sets[5]) > 7):
             print("SMALLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLER")
-            if abs(sets[2] - sets[1]) < 7:
+            if sets[2] < 40:
                 a = sets[1]+0.5
                 b = sets[2]
             else:
@@ -47,7 +48,7 @@ try:
             ans = a * math.sin(math.pi - sita) / math.sin(sita - math.pi * 25 / 180)
             sets[3] = round(ans, 1)
 
-        print([sets[0], sets[1], sets[2], sets[3], sets[4], sets[5], sets[6]], sita)
+        print([sets[0], sets[1], sets[2], sets[3], sets[4], sets[5], sets[6]], round(math.degrees(sita), 1))
         # distance = normalize(distance)
 except KeyboardInterrupt:
     pi.serial_close(h1)
