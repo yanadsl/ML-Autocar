b973f477c4a7add8967276805c1e2ac681637e5a
yanadsl <yanadsl1@hotmail.com>
2017-04-30 17:14:40 +0800

    fix some settings

--- test.py
+++ test.py
@@ -9,19 +9,24 @@ def set_speed(lspeed, rspeed):
 
 
 pi = pigpio.pi()
-
+h1 = pi.serial_open("/dev/ttyAMA0", 9600)
 left_servo_pin = 13
 right_servo_pin = 12
 dead_pin = 17
+die_distance = 8
 rs = 100
 ts = 100
 print("start")
+pi.serial_write_byte(h1, die_distance * 2)
+
 try:
     while True:
         set_speed(rs, ts)
         if pi.read(dead_pin) == pigpio.LOW:
             set_speed(0, 0)
+            pi.serial_close(h1)
 
 except KeyboardInterrupt:
     set_speed(0, 0)
+    pi.serial_close(h1)
     sys.exit(0)
