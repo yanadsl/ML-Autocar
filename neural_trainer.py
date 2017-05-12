612d33447c15f3e341badd8e36e2bef9179c7238
yanadsl <yanadsl1@hotmail.com>
2017-05-12 14:33:40 +0800

    fix

--- neural_trainer.py
+++ neural_trainer.py
@@ -1,20 +1,34 @@
 import RpiEnv
 import numpy as np
 
+yeah = False
 env = RpiEnv.Env()
+y_train = np.array([])
+x_train = np.array([])
+try:
+    y_train = np.loadtxt('y_train.txt', dtype=float)
+    x_train = np.loadtxt('x_train.txt', dtype=int)
+    yeah = True
+except:
+    print('Nda')
 input('準備讀取超音波 (請按Enter)')
 data = env.get_respond()
 distance = []
 for a in data:
     distance.append(int(a) / 2.0)
-distance = np.array(distance, np.float32)
+print(distance)
+distance = np.array([distance], np.float32)
 s = input('輸入正確答案:')
-l = np.array([i*0 for i in range(7)], np.int8)
+l = np.array([[i*0 for i in range(7)]], np.int8)
 for a in s:
     if a.isnumeric():
-        l[int(a)-1] = 1
-x_train = np.stack((distance))
-y_train = np.stack((l))
+        l[0][int(a)-1] = 1
+if yeah:
+    x_train = np.concatenate((x_train,distance))
+    y_train = np.concatenate((y_train,l))
+else:
+    x_train = np.concatenate((distance))
+    y_train = np.concatenate((l))
 
 while True:
     input('準備讀取超音波 (請按Enter)')
@@ -22,51 +36,16 @@ while True:
     distance = []
     for a in data:
         distance.append(int(a) / 2.0)
-    distance = np.array(distance, np.float32)
+    distance = np.array([distance], np.float32)
+
     s = input('輸入正確答案:')
     if str(s) == 'q' or str(s) == 'quit':
-        break;
-    l = np.array([i * 0 for i in range(7)], np.int8)
+        break
+    l = np.array([[i * 0 for i in range(7)]], np.int8)
     for a in s:
         if a.isnumeric():
-            l[int(a) - 1] = 1
-    x_train = np.stack((x_train, distance))
+            l[0][int(a) - 1] = 1
+    x_train = np.concatenate((x_train, distance))
     np.savetxt('x_train.txt', x_train, fmt='%d')
-    y_train = np.stack((y_train, l))
+    y_train = np.concatenate((y_train, l))
     np.savetxt('y_train.txt', y_train, fmt='%d')
-
-from keras.datasets import mnist
-from keras.utils import np_utils
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-from keras.optimizers import RMSprop, SGD
-
-# Another way to build your neural net
-model = Sequential([
-    Dense(8, input_dim=7),
-    Activation('sigmoid'),
-    Dense(4),
-    Activation('sigmoid')
-])
-
-# Another way to define your optimizer
-rmsprop = RMSprop(lr=0.1, rho=0.9, epsilon=1e-08, decay=0.0)
-# rmsprop = RMSprop(lr=0.1)
-sgd = SGD(lr=0.5, decay=0, momentum=0.9, nesterov=True)
-
-# We add metrics to get more results you want to see
-model.compile(optimizer=rmsprop,
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
-
-print('Training ------------')
-# Another way to train the model
-model.fit(x_train, y_train, epochs=100, batch_size=32)
-
-print('\nTesting ------------')
-# Evaluate the model with the metrics we defined earlier
-loss, accuracy = model.evaluate(x_train, y_train)
-
-print('test loss: ', loss)
-print('test accuracy: ', accuracy)
-model.save('sonic_predict.h5')
